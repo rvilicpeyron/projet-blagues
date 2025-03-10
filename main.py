41a7d0cda9c4c6efbb350ee077d541ff0de0b182
Zoe <zoe.abadie@esme.fr>
2025-03-10 13:40:44 +0100

    Modification dans branche_a

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def tell_joke(lang="en"):
     print(pyjokes.get_joke(language=lang))
 
 if __name__ == "__main__":
-    lang = input("Choisissez une langue (en, de, es, it, gl): ")
+    lang = input("Choisissez une langue (en, de, es, it, gl, fr): ")
     joke_type = input("Choisissez une catégorie de blague (neutral, chuck, all): ")
     tell_joke(lang, joke_type)
 
